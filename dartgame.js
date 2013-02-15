9c85918c4cd0a0b1862d551da0e01bca775f0b06
Petit Agité <petit.agitay@gmail.com>
2013-02-15 14:43:16 +0100

    Completed x01 base class and implemented 501 Added confirm behaviour

--- dartgame.js
+++ dartgame.js
@@ -13,33 +13,53 @@ var DartGame = {};
         gridContainerId: 'scoreboard',
         typeContainerId: 'type',
         turnsContainerId: 'turns',
+        dartNumber: 3,
         grid: null,
         _initBase: function(config) {
             if(config) {
                 for(var key in config) {
-                    this.key = config.key;
+                    this[key] = config[key];
                 }
             }
         },
         addPlayer: function(name) {
             return this.players.push(name) - 1;
         },
-        setScore: function(score) {
-            this.playerScore[this.currentPlayer][this.currentTurn] = score;
-            this._setGrid(score);
+        score: function(score) {
+            this._setScore(this._convertScore(this._getScore(score)));
+            this.next();
         },
         next: function() {
             this.currentPlayer = ++this.currentPlayer % this.players.length;
             if(this.currentPlayer == 0)
-                this.newTurn();
-        },
-        newTurn: function() {
-            this.currentTurn++;
-            return this.currentTurn > this.maxTurns;
+                this._newTurn();
         },
         start: function() {
             this._initGrid();
             this._displayGrid();
+        },
+        _newTurn: function() {
+            this.currentTurn++;
+            return this.currentTurn > this.maxTurns;
+        },
+        _getScore: function(score) {
+            var child = score.firstChild;
+            var result = [];
+            var dart = 0;
+            while(child) {
+                if(child.nodeName == 'LI')
+                    result[dart++] = { value: child.attributes['value'].nodeValue, multiplicator: child.attributes['multiplicator'].nodeValue, };
+                child = child.nextSibling;
+            }
+
+            return result;
+        },
+        _convertScore: function(score) {
+            var result = {};
+            result.total = 0;
+            for(var i in score)
+                result.total += score[i].value * score[i].multiplicator;
+            return result;
         },
         _initGrid: function() {
             this.grid = document.createElement("div");
@@ -50,6 +70,12 @@ var DartGame = {};
             document.getElementById(this.turnsContainerId).innerHTML = this.currentTurn + '/'  + this.maxTurns;
             document.getElementById(this.gridContainerId).innerHTML = "";
             document.getElementById(this.gridContainerId).appendChild(this.grid);
+        },
+        _setScore: function(score) {
+            if(!this.playerScore[this.currentPlayer])
+                this.playerScore[this.currentPlayer] = [];
+            this.playerScore[this.currentPlayer][this.currentTurn] = score;
+            this._setGrid(score);
         },
         _setGrid: function(score) {
             if(this.grid)
@@ -86,7 +112,31 @@ var DartGame = {};
 
                 this._setGrid({ turn: '-', total: this.maxScore, });
             }
-        }
+
+            this._newTurn();
+        },
+        _convertScore: function(score) {
+            var result = {};
+            if(this.currentTurn == 1)
+                result.total = this.maxScore;
+            else
+                result.total = this.playerScore[this.currentPlayer][this.currentTurn - 1].total;
+            result.turn = 0;
+
+            for(var i in score) {
+                result.turn += score[i].value * score[i].multiplicator;
+            }
+
+            if(result.total >= result.turn)
+                result.total -= result.turn;
+
+            return result;
+        },
+        _newTurn: function() {
+            this.currentTurn++;
+            document.getElementById(this.turnsContainerId).innerHTML = this.currentTurn + '/'  + this.maxTurns;            this.currentRow = this.grid.insertRow(-1);
+            return this.currentTurn > this.maxTurns;
+        },
     };
     Kinetic.Global.extend(DartGame.X01, DartGame.Base);
 
@@ -95,7 +145,7 @@ var DartGame = {};
     };
     DartGame.Five01.prototype = {
         _init501: function() {
-            DartGame.X01.call(this, { gameName: '501', maxTurns: '-', maxScore: 501 });
+            DartGame.X01.call(this, { gameName: 'Five-O-One', maxTurns: '-', maxScore: 501 });
         },
     };
     Kinetic.Global.extend(DartGame.Five01, DartGame.X01);
